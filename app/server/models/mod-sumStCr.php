73d38bca2681dcfdaef2e4184252a60ff5b2ebc1
chaim1 <cohen1ch@gmail.com>
2018-11-29 09:48:21 +0200

    add model modelStandCo

---
+++ app/server/models/mod-sumStCr.php
@@ -0,0 +1,31 @@
+<?php  
+require_once 'app/server/bl/bl-students.php';
+require_once 'app/server/bl/bl-courses.php';
+
+
+    class ModelStAndCr 
+    {
+        private $stdent_id;        
+        private $course_id;     
+                
+        
+        function __construct($arr) {
+
+            $this->stdent_id = $arr['stdent_id'];
+
+            $this->course_id = $arr['course_id'];      
+            
+        }
+
+        //get
+
+        public function getStdent_id() {
+            return $this->stdent_id;
+        }
+
+        public function getCourse_id() {
+            return $this->course_id;
+        }
+
+    }
+?>
