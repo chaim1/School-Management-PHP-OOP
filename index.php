c56b9697324ca94e7bf6da051bbbc46bf1cc598d
chaim1 <cohen1ch@gmail.com>
2018-11-30 15:00:54 +0200

    update

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     }
    
     if(isset($_POST['LoginLogin'])){
-        if(!empty($_POST['userNameLogin']&&!empty($_POST['pwdLogin']))){
+        if(!empty($_POST['userNameLogin'] && !empty($_POST['pwdLogin']))){
         
             $userName = $_POST['userNameLogin'];
             $password = $_POST['pwdLogin'];
@@ -94,7 +94,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
     <!-- Bootstrap core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    
+
     <link rel="stylesheet" href="test.css">
 
   </head>
