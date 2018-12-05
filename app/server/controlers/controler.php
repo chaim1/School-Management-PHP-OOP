4b88d7dfdf4f73bb660e2ad987e16ef3e8d27450
chaim1 <cohen1ch@gmail.com>
2018-12-05 09:55:53 +0200

    update

--- app/server/controlers/controler.php
+++ app/server/controlers/controler.php
@@ -3,15 +3,38 @@
 
 //$this function accept  file and position and she put the tile
         public function ActionInsertImage($position,$tempFile){
-
             $fileName = $tempFile[key($tempFile)]['name'];
+
             $fileExt = explode('.',$fileName);
+
             $fileActualExt = strtolower(end($fileExt));
+
             $fileTmpName = $tempFile[key($tempFile)]['tmp_name'];
+
             $fileNewName = uniqid('', true).".". $fileActualExt;
+
             $fileDastntion = $position.$fileNewName;
-            move_uploaded_file($fileTmpName,$fileDastntion);
-            return $fileNewName;
+
+            $fileSise = $tempFile[key($tempFile)]['size'];
+
+            $fileEror = $tempFile[key($tempFile)]['error'];
+        
+            $fileType = $tempFile[key($tempFile)]['type'];
+        
+            $allowed = array('jpg','jpeg','png');
+        
+            if(in_array($fileActualExt,$allowed)){
+        
+                if($fileEror===0){
+                
+                    if($fileSise<2000000){
+                        move_uploaded_file($fileTmpName,$fileDastntion);
+                        return $fileNewName;
+                    }else{}
+                }else{}
+            }else{}
+        
+            
         }
     }
 ?>
