01807b3375c522a8e15df074d4f99dd1283ee686
chaim1 <cohen1ch@gmail.com>
2018-12-17 21:05:40 +0200

    update

--- index.php
+++ index.php
@@ -300,7 +300,8 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>ProjectSchool</title>
+    <link rel="shortcut icon" type="image/png" href="jbLogo.png"/>
+    <title>ProjectSchoolChaim</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
